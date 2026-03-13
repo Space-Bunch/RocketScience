c3375e9d589b48f54acb3e5965c21c34dbec27c4
purcol <wwelol1234567@gmail.com>
2026-03-13 16:01:29 +0300

    austronafts

--- js/astronauts.js
+++ js/astronauts.js
@@ -5,7 +5,7 @@ function html_card(title="",desc="",date="",img="") {
             <div class="history_card_description">
                 <div style="height: 10px; display: grid; grid-template-columns: 1fr 1fr 4fr;">
                     <hr class="yellow" style="grid-column: 1;"></hr>
-                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px;">${date}</p>
+                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px; font-size:105%"><b>${date}</b></p>
                     <hr class="yellow" style="grid-column: 3;"></hr>
                 </div>
             <p style="margin-left: 10px;">${desc}</p></div></div>
@@ -29,7 +29,7 @@ function add_card(json="test.json"){
 
 window.onload = function() {
     // ВСТЯВЛЯТЬ ИМЕНА СЮДА \/ \/
-    const cards = ["test","test2","test3","test4"].reverse()
+    const cards = ["uri", "amstrong",,"frank","zigmyt","harison","alex","leon","titov","ter"].reverse()
     for (let card = cards.length; card--; card > 0){
         add_card(`${cards[card]}.json`)
     }
