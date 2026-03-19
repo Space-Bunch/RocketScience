4f221861a32d68c5c0e2d6cb2b04b54421a9647f
purcol <wwelol1234567@gmail.com>
2026-03-19 23:38:26 +0300

    ter.png replaced with proper placeholders and added hardcoded bottom padding for all info pages

--- js/future.js
+++ js/future.js
@@ -1,11 +1,11 @@
 function html_card(title="",desc="",date="",img="") {
     let html = `<div class="history_card midium yellow">
         <div style="grid-column: 1; grid-row: 2;">
-            <h2>${title}</h2>
+            <h2> </h2>
             <div class="history_card_description">
-                <div style="height: 10px; display: grid; grid-template-columns: 1fr 1fr 4fr;">
+                <div style="height: 10px; display: grid; grid-template-columns: 1fr 1.5fr 4fr;">
                     <hr class="yellow" style="grid-column: 1;"></hr>
-                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px;">${date}</p>
+                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px;"><b>${title}</b></p>
                     <hr class="yellow" style="grid-column: 3;"></hr>
                 </div>
             <p style="margin-left: 10px;">${desc}</p></div></div>
