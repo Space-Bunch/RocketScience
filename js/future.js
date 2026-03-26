4009cc64fd54fd51efbe031ebb0aefa928d98126
purcol <wwelol1234567@gmail.com>
2026-03-26 18:04:12 +0300

    more imbed

--- js/future.js
+++ js/future.js
@@ -8,20 +8,34 @@ function get_font_size(){
     return font_size;
 }
 
-function html_card(title="",desc="",date="",img="",size="midium") {
+function html_card(title="",desc="",date="",img="",size="midium",json="") {
     let html = `<div class="history_card ${size} yellow">
         <div style="grid-column: 1; grid-row: 2;">
             <h2> </h2>
             <div class="history_card_description">
                 <div style="height: 10px; display: grid; grid-template-columns: 1fr 1.5fr 4fr;">
                     <hr class="yellow" style="grid-column: 1;"></hr>
-                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px;  text-wrap: nowrap; ${get_font_size()}"><b>${title}</b></p>
+                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px;  text-wrap: nowrap; ${get_font_size()}">
+                        <button class="static yellow" popovertarget="${json+'_sourse'}"><b>${title}</b></button>
+                    </p>
                     <hr class="yellow" style="grid-column: 3;"></hr>
                 </div>
             <p style="margin-left: 10px; ${get_font_size()}">${desc}</p></div></div>
         <div style="grid-column: 2; grid-row: 2; width: 100%; height: 100%;">
             <img src="../resources/${img}" class="yellow" style="height: 200px; border-radius: 5px; border-width: 5px; border-style: solid;"></img>
         </div></div>`
+    const templete = document.createElement("template");
+    templete.innerHTML = html.trim();
+    return templete.content.firstElementChild;
+}
+
+function html_card_souse(json="",soures="") {
+    let html = `
+    <button id="${json+'_sourse'}" class="card_sourse static transperent_yellow" popover popovertarget="${json+'_sourse'}">
+        <div class="yellow" style="top: 10%; height: 80%; border-radius: 2px; outline-width: 5px; outline-style: solid;">
+            <iframe style="width: 100%; height: 100%;" src="${soures}"></iframe><br>
+        </div>
+    </button>`
     const templete = document.createElement("template");
     templete.innerHTML = html.trim();
     return templete.content.firstElementChild;
@@ -34,7 +48,8 @@ function add_card(json="test.json"){
             if (!response.ok){throw new Error(`HTTP error! Status: ${response.status}`);}
             return response.json();
         })
-        .then(data => root_element.appendChild(html_card(data.title,data.description,data.date,data.img,data.size)));
+        .then(data => root_element.appendChild(html_card(data.title,data.description,data.date,data.img,data.size,json)))
+        .then(data => root_element.appendChild(html_card_souse(json,data.sourse)));
 }
 
 window.onload = function() {
--- js/staged_rocket.js
+++ js/staged_rocket.js
@@ -8,7 +8,7 @@ function get_font_size(){
     return font_size;
 }
 
-function html_card(title="",desc="",date="",img="",size="exstra") {
+function html_card(title="",desc="",date="",img="",size="exstra",json="") {
     let html = `<div class="baner_card ${size} green">
         <div style="margin-left: 20%; margin-right: 20%; margin-bottom: 0;">
             <img src="../resources/${img}" class="green" style="height: calc(width * 0.779014); width: 53.542vw; border-radius: 5px; border-width: 5px; border-style: solid;"></img>
@@ -17,11 +17,25 @@ function html_card(title="",desc="",date="",img="",size="exstra") {
             <div class="history_card_description">
                 <div style="height: 10px; display: grid; grid-template-columns: 1fr 1fr 1fr;">
                     <hr class="green" style="grid-column: 1;"></hr>
-                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px; text-align: center; font-size: large; text-wrap: nowrap; ${get_font_size()}"><b> ${title}</b></p>
+                    <p style="grid-column: 2; margin-left: 30px; margin-right: 30px; margin-top: 7px; text-align: center; font-size: large; text-wrap: nowrap; ${get_font_size()}">
+                        <button class="static green" popovertarget="${json+'_sourse'}"><b>${title}</b></button>
+                    </p>
                     <hr class="green" style="grid-column: 3;"></hr>
                 </div>
             <p style="margin-left: 10px; ${get_font_size()}">${desc}</p></div></div>
         </div>`
+    const templete = document.createElement("template");
+    templete.innerHTML = html.trim();
+    return templete.content.firstElementChild;
+}
+
+function html_card_souse(json="",soures="") {
+    let html = `
+    <button id="${json+'_sourse'}" class="card_sourse static transperent_green" popover popovertarget="${json+'_sourse'}">
+        <div class="green" style="top: 10%; height: 80%; border-radius: 2px; outline-width: 5px; outline-style: solid;">
+            <iframe style="width: 100%; height: 100%;" src="${soures}"></iframe><br>
+        </div>
+    </button>`
     const templete = document.createElement("template");
     templete.innerHTML = html.trim();
     return templete.content.firstElementChild;
@@ -34,7 +48,8 @@ function add_card(json="test.json"){
             if (!response.ok){throw new Error(`HTTP error! Status: ${response.status}`);}
             return response.json();
         })
-        .then(data => root_element.appendChild(html_card(data.title,data.description,data.date,data.img,data.size)));
+        .then(data => root_element.appendChild(html_card(data.title,data.description,data.date,data.img,data.size,json)))
+        .then(data => root_element.appendChild(html_card_souse(json,data.sourse)));;
 }
 
 window.onload = function() {
